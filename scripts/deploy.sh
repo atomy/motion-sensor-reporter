79ff18a0d0bebc3c7ed1d95e74192dfc001902fb
Marcel Lamm <marcel.lamm@jackinpoint.net>
2020-10-18 13:53:27 +0200

    - adding pi-kitchen to deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,6 @@
 
 set -e
 
-#echo "Deploying to pi-kitchen@192.168.2.22..."
-#scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@192.168.2.22:~/.docker/config.json
-#scp -o StrictHostKeyChecking=no docker-compose.yml pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/docker-compose.yml
-#scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/deployInner.sh
-#scp -o StrictHostKeyChecking=no scripts/notification.sh pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/notification.sh
-#scp -o StrictHostKeyChecking=no changes pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/changes
-#ssh -o StrictHostKeyChecking=no pi@192.168.2.22 "cd /home/pi/apps/motion-sensor-reporter && ./deployInner.sh"
-#echo "Deploying to pi-kitchen@192.168.2.22...DONE"
-
 echo "Deploying to pi-office@192.168.2.21..."
 scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@192.168.2.21:~/.docker/config.json
 scp -o StrictHostKeyChecking=no docker-compose.yml pi@192.168.2.21:/home/pi/apps/motion-sensor-reporter/docker-compose.yml
@@ -20,3 +11,13 @@ scp -o StrictHostKeyChecking=no changes pi@192.168.2.21:/home/pi/apps/motion-sen
 scp -o StrictHostKeyChecking=no current_version pi@192.168.2.21:/home/pi/apps/motion-sensor-reporter/current_version
 ssh -o StrictHostKeyChecking=no pi@192.168.2.21 "cd /home/pi/apps/motion-sensor-reporter && ./deployInner.sh"
 echo "Deploying to pi-office@192.168.2.21...DONE"
+
+echo "Deploying to pi-kitchen@192.168.2.22..."
+scp -o StrictHostKeyChecking=no ~/.docker/config.json pi@192.168.2.22:~/.docker/config.json
+scp -o StrictHostKeyChecking=no docker-compose.yml pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/docker-compose.yml
+scp -o StrictHostKeyChecking=no scripts/deployInner.sh pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/deployInner.sh
+scp -o StrictHostKeyChecking=no scripts/notification.sh pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/notification.sh
+scp -o StrictHostKeyChecking=no changes pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/changes
+scp -o StrictHostKeyChecking=no current_version pi@192.168.2.22:/home/pi/apps/motion-sensor-reporter/current_version
+ssh -o StrictHostKeyChecking=no pi@192.168.2.22 "cd /home/pi/apps/motion-sensor-reporter && ./deployInner.sh"
+echo "Deploying to pi-kitchen@192.168.2.22...DONE"
